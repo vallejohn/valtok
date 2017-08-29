2ae15a50f81cdc369550d6eae74c163edc02f235
vallejohn <valjohn.teruel@gmail.com>
2017-08-29 16:11:55 +0900

    Customized navbav and added about page

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,33 @@
+@extends('master')
+
+@section('content')
+    <div class="container pt-5 text-center" style="letter-spacing: px">
+        <div class="col-lg-8 mx-auto">
+            <img src="https://pbs.twimg.com/profile_images/867573457550233600/KxiwEfuD.jpg" class="rounded-circle"
+                 style="width: 150px; height: 150px; border: 4px solid #ffffff">
+            <h3 class="mt-2">Valle John R. Teruel</h3>
+            <p class="text-justify pt-5"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed a tellus nibh. Aliquam varius volutpat luctus.
+                Curabitur quis eros porta, efficitur purus in, sagittis ligula. Aenean imperdiet arcu in scelerisque
+                sagittis. Pellentesque mattis nisi id enim rhoncus, quis vestibulum quam feugiat. Curabitur vulputate
+                blandit aliquet. Vivamus hendrerit feugiat orci quis varius. Pellentesque habitant morbi tristique senectus
+                et netus et malesuada fames ac turpis egestas. Maecenas ullamcorper enim quis nulla lacinia pellentesque.
+                Quisque nec efficitur eros.
+
+                Pellentesque a lectus turpis. Ut a leo quis lectus ullamcorper dignissim at eu turpis. Suspendisse dui odio,
+                vestibulum sit amet suscipit nec, pellentesque ut neque. Ut nec purus interdum, ultrices enim id, semper
+                sapien. Nunc non facilisis nunc. Curabitur et pulvinar est. In hac habitasse platea dictumst. Pellentesque
+                tristique tincidunt purus, quis iaculis dolor euismod et. Curabitur eget eleifend risus, vel lobortis justo.
+                Curabitur ultricies consequat lorem vitae consectetur. In tempor eleifend odio, eget viverra velit hendrerit
+                eu.
+
+                Vestibulum lobortis interdum hendrerit. Cras ut elit id lacus molestie hendrerit. Sed orci ipsum, ultrices
+                at nunc id, placerat dapibus diam. Morbi sed purus bibendum, vulputate dui non, semper mi. Sed eget justo
+                convallis odio egestas mollis. Donec viverra posuere libero, consectetur faucibus lectus elementum ac. Nunc
+                porttitor dignissim molestie. Suspendisse dignissim fringilla justo et vestibulum. Nunc risus mi, cursus
+                eget tristique vel, iaculis ut neque. Praesent lacinia in nisl vitae dignissim. Nulla cursus dignissim massa
+                non vestibulum. Vivamus vulputate at urna at lobortis. Lorem ipsum dolor sit amet, consectetur adipiscing
+                elit. Mauris efficitur, nibh vitae tincidunt euismod, eros orci egestas purus, vitae tristique diam nisl at
+                ante. Etiam quis posuere nulla, vitae ultrices eros. Nulla facilisi. </p>
+        </div>
+    </div>
+@endsection
---
+++ resources/views/partials/_nav.blade.php
@@ -0,0 +1,22 @@
+<nav class="navbar navbar-toggleable-md navbar-light">
+    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <a class="navbar-brand hidden-lg-up" href="#">Val Teruel</a>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul class="navbar-nav mx-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">ABOUT <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">BLOG</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">PORTFOLIO</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">CONTACT</a>
+            </li>
+        </ul>
+    </div>
+</nav>
